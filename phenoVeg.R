a8b2230f2548bd651d91a7928fba590d86730117
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2021-11-28 19:32:07 -0500

    Create phenoVeg.R

---
+++ phenoVeg.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+
+setwd("/scratch/ahc87874/Fall2021Practice/Project")
+
+source("pheno.R")
+
+vegqc3<-read.delim("vegQC2_04032021.txt", header=TRUE, sep="\t")
+
+#Inner join
+participants2<-as_tibble(inner_join(participants1, vegqc3, by="IID"))
+new2<-as_tibble(inner_join(new, vegqc3, by="IID"))
+
+#Output
+outdir="/scratch/ahc87874/Fall2021Practice/Project"
+
+#Model 1
+write.table(participants2, 
+	paste(outdir, "/PUFA_GWAS_phenoQC_IDS_M1_Veg.txt",sep=""), 
+	row.names=FALSE, quote=FALSE)
+
+write.table(new2, 
+	paste(outdir, "/PUFA_GWAS_pheno_M1_Veg.txt", sep=""),
+	row.names=FALSE, quote=FALSE)
