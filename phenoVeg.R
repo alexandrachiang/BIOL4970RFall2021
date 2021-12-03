bb94e21a810561b00567134e03f574e170f45a00
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2021-12-03 12:53:43 -0500

    Update phenoVeg.R

--- phenoVeg.R
+++ phenoVeg.R
@@ -1,27 +1,42 @@
-#Add vegetarian columns
-
 library(tidyverse)
 
 setwd("/scratch/ahc87874/Fall2021Practice/Project")
 
 source("pheno.R")
 
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#Add vegetarian columns
 vegqc3<-read.delim("vegQC2_04032021.txt", header=TRUE, sep="\t")
 
 #Inner join
 participants2<-as_tibble(inner_join(participants1, vegqc3, by="IID"))
 new2<-as_tibble(inner_join(new, vegqc3, by="IID"))
 
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#PCs
+pan<-read_tsv("/scratch/ahc87874/Fall2021Practice/Project/all_pops_non_eur_pruned_within_pop_pc_covs.tsv")
+pan<-as_tibble(pan)
+pan$s<-as.integer(pan$s)
+table(pan$pop, useNA = "always")
+
+#Inner join
+participants3<-as_tibble(inner_join(participants2, pan, by="IID"))
+new3<-as_tibble(inner_join(new2, pan, by="IID"))
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 #Output
 outdir="/scratch/ahc87874/Fall2021Practice/Project"
 
-write.table(participants2, 
+write.table(participants3, 
 	paste(outdir, "/GWAS_phenoQC_IDS_M1_Veg.txt",sep=""), 
 	row.names=FALSE, quote=FALSE)
 
-write.table(new2, 
+write.table(new3, 
 	paste(outdir, "/GWAS_pheno_M1_Veg.txt", sep=""),
 	row.names=FALSE, quote=FALSE)
 
-write.csv(new2, 
+write.csv(new3, 
 	paste(outdir, "/GWAS_pheno_M1_Veg.csv")
