32510e88d9f7fa88adb2e3dd1b920031bc9cce19
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2021-12-04 13:56:12 -0500

    Update phenoVeg.R

--- phenoVeg.R
+++ phenoVeg.R
@@ -10,7 +10,6 @@ source("pheno.R")
 vegqc3<-read.delim("vegQC2_04032021.txt", header=TRUE, sep="\t")
 
 #Inner join
-participants2<-as_tibble(inner_join(participants1, vegqc3, by="IID"))
 new2<-as_tibble(inner_join(new, vegqc3, by="IID"))
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -29,22 +28,29 @@ pan2<-pan%>%select(s, pop, PC1, PC2, PC3, PC4, PC5, PC6, PC7, PC8, PC9, PC10)%>%
     left_join(bridge, by=c("s"="panID"))
 
 #Inner join
-participants3<-as_tibble(inner_join(participants2, pan2, by="IID"))
 new3<-as_tibble(inner_join(new2, pan2, by="IID"))
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-participants3 <- participants3 %>% select("FID", "IID")
+#Complete cases
+new4<-new3[complete.cases(new3),]
+#removes 26,173
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+participants <- new4 %>% select("FID", "IID")
+participants$FID<-participants$IID
+participants<-participants%>%select(FID, IID)
 
 #Output
 outdir="/scratch/ahc87874/Fall2021Practice/Project/pheno"
 
-write.table(participants3, 
+write.table(participants, 
 	paste(outdir, "/GWAS_phenoQC_IDS_M1_Veg.txt", sep=""), 
 	row.names=FALSE, quote=FALSE)
 
-write.table(new3, 
+write.table(new4, 
 	paste(outdir, "/GWAS_pheno_M1_Veg.txt", sep=""),
 	row.names=FALSE, quote=FALSE)
 
-write.csv(new3, "GWAS_pheno_M1_Veg.csv", quote=FALSE, row.names=FALSE)
+write.csv(new4, "GWAS_pheno_M1_Veg.csv", quote=FALSE, row.names=FALSE)
