5eabf17263fd45b4a99b56a0a7295db67b20217b
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2021-12-06 16:08:44 -0500

    Update meltgraphs.R

--- meltgraphs.R
+++ meltgraphs.R
@@ -227,12 +227,12 @@ graphCSRVHDL<- ggplot(data = CSRVHDL, aes(x = Sex, y = value, fill = CSRV, color
          "\n n =", nrow(CSRVHDL)/2,
          sep = " ")) +
   stat_summary(fun = mean, geom = "point", shape = 18, size = 3) +
-  annotate("text", x = 2.5, y = meannonveg1-1, label = meannonveg1, color = "#F8766D") +
-  annotate("text", x = 1.5, y = meannonvegF1+1, label = meannonvegM1, color = "#F8766D") +
-  annotate("text", x = 0.5, y = meannonvegM1+1, label = meannonvegF1, color = "#F8766D") +
-  annotate("text", x = 2.5, y = meanveg1+1, label = meanveg1, color = "#00BA38") +
-  annotate("text", x = 1.5, y = meanvegF1-1, label = meanvegM1, color = "#00BA38") +
-  annotate("text", x = 0.5, y = meanvegM1-1, label = meanvegF1, color = "#00BA38") +
+  annotate("text", x = 2.5, y = meannonveg1-.3, label = meannonveg1, color = "#F8766D") +
+  annotate("text", x = 1.5, y = meannonvegF1+.6, label = meannonvegM1, color = "#F8766D") +
+  annotate("text", x = 0.5, y = meannonvegM1, label = meannonvegF1, color = "#F8766D") +
+  annotate("text", x = 2.5, y = meanveg1+.3, label = meanveg1, color = "#00BA38") +
+  annotate("text", x = 1.5, y = meanvegF1, label = meanvegM1, color = "#00BA38") +
+  annotate("text", x = 0.5, y = meanvegM1-.6, label = meanvegF1, color = "#00BA38") +
   scale_fill_manual(values = c("#F8766D", "#00BA38")) +
   scale_color_manual(values = c("#F8766D", "#00BA38")) +
   scale_x_discrete(labels = xlab1) + 
@@ -264,12 +264,12 @@ graphSSRVHDL<- ggplot(data = SSRVHDL, aes(x = Sex, y = value, fill = SSRV, color
          "\n n =", nrow(SSRVHDL)/2,
          sep = " ")) +
   stat_summary(fun = mean, geom = "point", shape = 18, size = 3) +
-  annotate("text", x = 2.5, y = meannonveg1+1, label = meannonveg1, color = "#F8766D") +
-  annotate("text", x = 1.5, y = meannonvegF1+1, label = meannonvegM1, color = "#F8766D") +
-  annotate("text", x = 0.5, y = meannonvegM1+1, label = meannonvegF1, color = "#F8766D") +
-  annotate("text", x = 2.5, y = meanveg1-1, label = meanveg1, color = "#00BA38") +
-  annotate("text", x = 1.5, y = meanvegF1-1, label = meanvegM1, color = "#00BA38") +
-  annotate("text", x = 0.5, y = meanvegM1-1, label = meanvegF1, color = "#00BA38") +
+  annotate("text", x = 2.5, y = meannonveg1+.3, label = meannonveg1, color = "#F8766D") +
+  annotate("text", x = 1.5, y = meannonvegF1+.6, label = meannonvegM1, color = "#F8766D") +
+  annotate("text", x = 0.5, y = meannonvegM1, label = meannonvegF1, color = "#F8766D") +
+  annotate("text", x = 2.5, y = meanveg1-.3, label = meanveg1, color = "#00BA38") +
+  annotate("text", x = 1.5, y = meanvegF1, label = meanvegM1, color = "#00BA38") +
+  annotate("text", x = 0.5, y = meanvegM1-.6, label = meanvegF1, color = "#00BA38") +
   scale_fill_manual(values = c("#F8766D", "#00BA38")) +
   scale_color_manual(values = c("#F8766D", "#00BA38")) +
   scale_x_discrete(labels = xlab1) + 
