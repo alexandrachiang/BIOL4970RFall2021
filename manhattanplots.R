d74a7951fdc6d07a32ed2f6b1681132df538dbdc
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2021-12-07 00:08:46 -0500

    Update manhattanplots.R

--- manhattanplots.R
+++ manhattanplots.R
@@ -6,24 +6,49 @@ indir="/scratch/ahc87874/Fall2021Practice/Project/GEM/TotalCholesterol"
 for (i in 1:22) {
 infile<-read.table(paste(indir, paste("TotalCholesterolxConsistent_Self_Reported_Vegetarian_across_all_24hr-chr", i, sep=""), sep="/"), header=TRUE, stringsAsFactors=FALSE)
 infile<-as_tibble(infile) 
-colnames(infile)<-c("CHROM", "POS", "ID", "REF", "ALT", "A1", "TEST", "OBS_CT", "BETA", "SE", "T_STAT", "P", "ERRCODE")
-
+  
+#names(infile)<-c("CHROM", "POS", "ID", "REF", "ALT", "A1", "TEST", "OBS_CT", "BETA", "SE", "T_STAT", "P", "ERRCODE")
+#[1] "SNPID"
+#[2] "RSID"
+#[3] "CHR"
+#[4] "POS"
+#[5] "Non_Effect_Allele"
+#[6] "Effect_Allele"
+#[7] "N_Samples"
+#[8] "AF"
+#[9] "N_Consistent_Self_Reported_Vegetarian_across_all_24hr_1"
+#[10] "AF_Consistent_Self_Reported_Vegetarian_across_all_24hr_1"
+#[11] "N_Consistent_Self_Reported_Vegetarian_across_all_24hr_0"
+#[12] "AF_Consistent_Self_Reported_Vegetarian_across_all_24hr_0"
+#[13] "Beta_Marginal"
+#[14] "robust_SE_Beta_Marginal"
+#[15] "Beta_G.Consistent_Self_Reported_Vegetarian_across_all_24hr"
+#[16] "robust_SE_Beta_G.Consistent_Self_Reported_Vegetarian_across_all_24hr"
+#[17] "robust_P_Value_Marginal"
+#[18] "robust_P_Value_Interaction"
+#[19] "robust_P_Value_Joint"
+  
 #Subset data
-infile1<-infile%>%filter(TEST=="ADD")%>%select(CHROM, POS, P, ID)
+infile1<-infile%>%select(CHR, POS, robust_P_Value_Joint, RSID)
 
 #Get qqman format
 colnames(infile1)<-c("CHR", "BP", "P", "SNP")
 
 #Add to input
-rbind(infileall, infile1)
+if (i == 1) {
+	infileall<-infile1
+} else {
+	infileall<-rbind(infileall, infile1)
+}
+
 }
 
 #Make manhattan plot
 plotoutputpath<-paste(indir, "/plot", sep="")
 dir.create(plotoutputpath, showWarnings=FALSE)
-plotoutputfile<-paste(plotoutputpath, "/chr1.22man.png", sep="")
+plotoutputfile<-paste(plotoutputpath, "/TotalCholesterolxConsistent_Self_Reported_Vegetarian_across_all_24hr.png", sep="")
 
 png(filename=plotoutputfile, type="cairo")
-manhattan(infileall, ylim=c(0,200))
+manhattan(infileall, ylim=c(0,200), col = c("firebrick1", "black"), cex = 0.6)
 dev.off()
 }
