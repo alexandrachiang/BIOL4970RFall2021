5e77c0a2b3e06eb46889c4f9a6b81997d1ac9f1f
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-01-19 16:10:23 -0500

    Update genoQC.sh

--- genoQC.sh
+++ genoQC.sh
@@ -21,14 +21,14 @@ ml PLINK/2.00-alpha2.3-x86_64-20210920-dev
 echo "-=-=-=-=-=-=-=-STEP 1-=-=-=-=-=-=-=-\n\n"
 
 genoindir=("/scratch/ahc87874/bgen_v1.2_UKBsource")
-mfiscoredir=("/scratch/ahc87874/Fall2021Practice/UKBpgen/mfi")
+mfiscoredir=("/scratch/ahc87874/Fall2021Practice/UKBpgen/mfi/info0.5")
 outdir=("/scratch/ahc87874/Fall2021Practice/Project/genoQC2")
 mkdir -p $outdir
 
 plink2 \
 --bgen $genoindir/ukb_imp_chr"$i"_v3.bgen ref-first \
 --sample $genoindir/ukb_imp_v3.sample \
---extract $mfiscoredir/ukb_mfi_chr"$i"_v3.txt \
+--extract $mfiscoredir/ukb_mfi_chr"$i"_v3_0.5.txt \
 --mind 0.05 \
 --geno 0.02 \
 --hwe 1e-06 \
